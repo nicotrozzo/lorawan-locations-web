9caa30de5f8948c8befcc477a052b4f7c7c6986a
nicotrozzo <rtrozzo@itba.edu.ar>
2021-05-28 20:17:09 +0200

    Point index.js to gonzi's html

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ let app;
     
     // Serve index.html for default route
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './index.html'));//'client/index.html'));
+        res.sendFile(path.join(__dirname, '/client/index.html'));//'client/index.html'));
     });
 
     app.listen(config.server.port, () => {
